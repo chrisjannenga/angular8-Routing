f6198a66408284dad685b921e55dc91c7f3ac2ae
Chris Jannenga <cjannenga312@gmail.com>
2019-06-12 14:11:23 -0700

    Routing Section of Angular 8 course

---
+++ src/app/servers/edit-server/can-deactivate-gaurd.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot} from '@angular/router';
+
+export interface CanComponentDeactivate {
+  canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CanDeactivateGuardService implements CanDeactivate<CanComponentDeactivate> {
+
+canDeactivate(component: CanComponentDeactivate,
+              currentRoute: ActivatedRouteSnapshot,
+              currentState: RouterStateSnapshot,
+              nextState?: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+    return component.canDeactivate();
+}
+
+  constructor() { }
+}
